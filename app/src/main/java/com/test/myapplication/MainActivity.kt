bbb2adb1148597f5517ca3b68646a9896658e20a
peavy1 <peavy@culturehero.net>
2025-09-23 14:22:51 +0900

    first commit

---
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.test.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.TooltipCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.test.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupJetpackNavigation()
+    }
+
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.booksearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragment_game, R.id.fragment_app, R.id.fragment_search, R.id.fragment_book
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        applyItemForegroundRipple()
+        suppressBottomNavLongClick()
+    }
+
+    private fun applyItemForegroundRipple() {
+        val menuView = binding.bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0 until menuView.childCount) {
+            val itemView = menuView.getChildAt(i) as BottomNavigationItemView
+
+//            val ripple = ContextCompat.getDrawable(this, R.drawable.bottom_nav_icon_ripple)
+            val ripple = ContextCompat.getDrawable(this, R.drawable.bottom_nav_icon_ripple)
+
+            itemView.foreground = ripple
+        }
+    }
+
+    private fun suppressBottomNavLongClick() {
+        val menuView = binding.bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0 until menuView.childCount) {
+            val itemView = menuView.getChildAt(i) as BottomNavigationItemView
+
+            itemView.setOnLongClickListener {
+                true
+            }
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
